054499a5b396bdb6bdda04fc53eec57d9cb9f7d7
Your Name <catarina.vi.pires@gmail.com>
2020-07-26 18:18:46 +0200

    button start/stop, bg color

--- app/src/main/java/com/example/minitronome/MainActivity.kt
+++ app/src/main/java/com/example/minitronome/MainActivity.kt
@@ -1,12 +1,28 @@
-package com.example.minitronome
+package com.geeksforgeeks.myfirstkotlinapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button?.setOnClickListener()
+        {
+            fun setStop() = if (button.text == "Start") {
+                button.setText("Stop")
+            }else{
+                button.setText("Start")
+            }
+
+            setStop()
+            Toast.makeText(this@MainActivity,"Button 1 clicked", Toast.LENGTH_LONG).show()
+        }
+
     }
 }
